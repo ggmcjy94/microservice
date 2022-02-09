507843775771c3ea9bb64e140a9e620a02ebeb36
ggmcjy <ggmcjy94@naver.com>
2022-02-09 18:14:40 +0900

    Initial commit

---
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -0,0 +1,32 @@
+package com.example.secondservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@RestController
+@RequestMapping("/second-service")
+public class SecondServiceController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the Second service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        log.info(header);
+        return "Hello World in Second Service.";
+    }
+
+    @GetMapping("/check")
+    public String check(HttpServletRequest request) {
+        log.info("server port = {}" , request.getServerPort());
+        return String.format("Hi. there. This is a message from Second Service On Port %s", request.getServerPort());
+    }
+}
---
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.example.userservice.controller;
+
+import com.example.userservice.vo.Greeting;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/")
+public class UserController {
+
+    private final Greeting greeting;
+    private final Environment env;
+
+    @GetMapping("/health_check")
+    public String status() {
+        return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return env.getProperty("greeting.message");
+    }
+
+    @GetMapping("/welcome2")
+    public String welcome2() {
+        return greeting.getMessage();
+    }
+
+}
