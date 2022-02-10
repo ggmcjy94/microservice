dca7df0ecfc9f0be16dcdfcb6b7bebb3860b84cb
ggmcjy <ggmcjy94@naver.com>
2022-02-10 14:56:39 +0900

    2022 2-10 user-service commit

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableDiscoveryClient //eureka server 에 등록
@@ -11,6 +13,13 @@ public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+
+	//spring 이 실행 될때 Context 에서 bean 을 등록 시켜 객체를 메모리에 저장 시킴 그래서 주입을 받을려면 빈을 등록 시켜주어야함
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
